46ce6b10f4e3fba17031c0e7b820e3ba80d01e8c
jordanflop <jordanflop3@gmail.com>
2023-05-11 19:59:13 -0400

    added the rizzard of ozz image into every website you visit
    just to test if the script element injection was working

--- chrome extension/scripts/inject.js
+++ chrome extension/scripts/inject.js
@@ -1,1 +1,7 @@
 console.log("whats up this is rizzerd of ozz bitch");
+
+const img = document.createElement("img")
+
+img.src = "https://i.postimg.cc/tRW33Hrd/wizard4.jpg"
+
+document.body.appendChild(img)
